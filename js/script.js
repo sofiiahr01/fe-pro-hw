84e05d679e933d559c74878cb8bf8e30836bce4b
sofiiahr01 <sgradinarova2001@gmail.com>
2024-07-31 20:38:45 +0300

    hw_2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+//Task 1
+let name;
+let admin;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+//Task 2
+let planetName = 'Earth';
+let siteVisitorName = 'Bob';
+
+//Task 3
+let value = "10" + 10;
+console.log(value);
+
+
