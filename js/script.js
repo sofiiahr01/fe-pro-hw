8e3dcda0a67277be18db81cba41af5316574805c
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-01 18:27:42 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let operand1 = prompt("Введіть перший операнд");
+let operand2 = prompt("Введіть другий операнд");
+
+let sum = +operand1 + +operand2;
+let difference = operand1 - operand2;
+let multiplication = operand1 * operand2;
+let division = operand1 / operand2;
+
+console.log(`Результат: ${operand1} + ${operand2} = ${sum}`);
+console.log(`Результат: ${operand1} - ${operand2} = ${difference}`);
+console.log(`Результат: ${operand1} * ${operand2} = ${multiplication}`);
+console.log(`Результат: ${operand1} / ${operand2} = ${division}`);
