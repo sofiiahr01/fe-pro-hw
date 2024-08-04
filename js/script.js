c10c8d4c6053b8a4d0a4f0e9fb973bb9486894cc
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-04 14:32:39 +0300

    hw_6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const name = prompt('Enter your name');
+const age = prompt('Enter your age');
+let likesProgramming = confirm('Do you like programming?');
+
+console.log(name, age, likesProgramming);
+
+console.log (typeof name);
+console.log (typeof age);
+console.log (typeof likesProgramming);
+
+let greeting = `Hello, ${name}!`;
+alert(greeting);
+
+let numericAge = Number(age);
+let nextAge = numericAge + 1;
+console.log(nextAge);
+
+let isAdult = age >= 18;
+console.log(isAdult);
