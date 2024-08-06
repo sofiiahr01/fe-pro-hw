57e10eaa883c4632c4653f89c4ccdc0e13ed02d5
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-07 01:14:10 +0300

    hw_8 completed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ const number1 = +prompt ('Enter first number');
 const number2 = +prompt ('Enter second number');
 const number3 = +prompt ('Enter third number');
 
+const average = (number1 + number2 + number3) / 3;
+
 if (isNaN(number1) || isNaN(number2) || isNaN(number3)) {
     alert('Please, enter valid numbers');
-} else {const average = (number1 + number2 + number3) / 3;
+} else {
+        alert(`Arithmetic mean is ${average}`);
         console.log(`(${number1} + ${number2} + ${number3}) / 3 = ${average}`);
-        alert(`Arithmetic mean is ${average}`)}
+}
