9c5267d2ddeaa7bcbd44947dffac98475c3b39b7
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-07 00:11:43 +0300

    hw_7 completed

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ const hours = +prompt ('Enter hours amount');
 const minutes = hours * 60;
 const seconds = minutes * 60;
 
+console.log(`${hours} * 60 = ${minutes}`);
+console.log(`${minutes} * 60 = ${seconds}`);
+
 alert(`${hours} hours is ${seconds} seconds`);
