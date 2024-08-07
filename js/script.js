d1b9bdd020dbbf973a90f591452b3ffb82714dc1
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-08 00:41:47 +0300

    hw_8 completed

--- js/script.js
+++ js/script.js
@@ -8,4 +8,8 @@ const seconds = minutes * 60;
 console.log(`${hours} * 60 = ${minutes}`);
 console.log(`${minutes} * 60 = ${seconds}`);
 
-alert(`${hours} hours is ${seconds} seconds`);
+if (hours !== null && !isNaN(hours)){
+    alert(`${hours} hours is ${seconds} seconds`);
+} else {
+    alert('Please, enter valid numbers');
+}
